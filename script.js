b5dc7efd772aa2ed0833355b2482392542918f29
Giorgi Londaridze <londara16@gmail.com>
2023-04-04 23:48:09 +0400

    fix: fix image positions and js code functionality

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let faqs = document.querySelectorAll(".faq");
+let activeFaq = null;
 
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
+        if (activeFaq && activeFaq !== faq) {
+            activeFaq.classList.remove("active");
+        }
         faq.classList.toggle("active");
+        activeFaq = faq.classList.contains("active") ? faq : null;
     });
 });
